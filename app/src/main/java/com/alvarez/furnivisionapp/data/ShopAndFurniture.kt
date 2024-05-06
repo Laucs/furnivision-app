61d0664db63e6622e4069d7875c0edf06775f611
Daniel Stephen Alvarez <159105408+DanStphn@users.noreply.github.com>
2024-05-07 01:40:45 +0800

    Cleaned Merge

--- app/src/main/java/com/alvarez/furnivisionapp/data/ShopAndFurniture.kt
+++
@@ -1,4 +0,0 @@
-package com.alvarez.furnivisionapp.data
-
-data class Shop(val id: String?, val shopName: String?, val location: String?)
-data class Furniture(val id: String?, val name: String?, val type: String?, val price: Double, val shop: Shop)
