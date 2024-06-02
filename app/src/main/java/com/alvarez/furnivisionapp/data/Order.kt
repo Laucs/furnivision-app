13c37ee8aa8bcab5a0dffa02022fd59b7c80c53c
Yancy Louis S. Luceño <yancy2boy@gmail.com>
2024-06-02 22:02:09 +0800

    Updates (Dashboard, Search)

--- app/src/main/java/com/alvarez/furnivisionapp/data/Order.kt
+++ app/src/main/java/com/alvarez/furnivisionapp/data/Order.kt
@@ -1,5 +1,7 @@
 package com.alvarez.furnivisionapp.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.Date
 
-data class Order(var userId: String, var furnitures: MutableList<String>, var date: Date)
+data class Order(var id: String, var userId: String, var items: MutableList<ShopCart>, var date: Date)
